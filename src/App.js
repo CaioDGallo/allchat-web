a8d11834707acaf057dce91869114e5b200d8b8b
Caio Delafiori Gallo <caio.gallo@arcasolutions.com>
2020-02-11 11:20:43 -0300

    Testing websocket connection

--- src/App.js
+++ src/App.js
@@ -8,29 +8,52 @@ import './Main.css';
 import ChatForm from './components/ChatForm';
 
 function App() {
-  const [devs, setDevs] = useState([]);
+  const [messages, setMessages] = useState('');
 
   useEffect(() => {
-    async function loadDevs() {
-      //const response = await api.get('/devs');
+    async function loadMessages() {
+      let socket = new WebSocket("ws://pure-bastion-70060.herokuapp.com/");
 
-      //setDevs(response.data);
+      socket.onopen = function (e) {
+        alert("[open] Connection established");
+        alert("Sending to server");
+        socket.send("My name is John");
+      };
+
+      socket.onmessage = function (event) {
+        setMessages(`Message received from server: ${event.data}`)
+      };
+
+      socket.onclose = function (event) {
+        if (event.wasClean) {
+          alert(`[close] Connection closed cleanly, code=${event.code} reason=${event.reason}`);
+        } else {
+          // e.g. server process killed or network down
+          // event.code is usually 1006 in this case
+          alert('[close] Connection died');
+        }
+      };
+
+      socket.onerror = function (error) {
+        alert(`[error] ${error.message}`);
+      };
     }
 
-    loadDevs();
+    loadMessages();
   }, []);
 
-  async function handleAddDev(data) {
+  async function handleSendMessage(data) {
     const response = await api.post('/devs', data)
 
-    setDevs([...devs, response.data]);
+    setMessages(response.data);
   }
 
   return (
     <div id="app">
       <main>
-      <strong>Chat</strong>
-        <ChatForm onSubmit={handleAddDev} />
+        <strong>Chat</strong>
+        <ChatForm onSubmit={handleSendMessage} />
+        <p>{messages}</p>
       </main>
     </div>
   );
